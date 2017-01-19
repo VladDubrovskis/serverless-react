bb0d2a71c2ceab7f0646f62b7ded95f0654ff90e
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-01-19 20:47:18 +0000

    Add react-router v4 release to the repository.
    Reason for using v4 release is it supports BrowserRouter and ServerRouter to serve JS and non-JS versions.
    As this is a longer term project and will not be available in production for a while the alpha release is not an issue.
    The hope is that by the time this solution matures the v4 of react-router will be released.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
 import React, { Component } from 'react';
+import { Match, Miss, Link } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
+
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topic = ({ params }) => (
+  <div>
+    <h3>{params.topicId}</h3>
+  </div>
+)
+
+const Topics = ({ pathname }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li><Link to={`${pathname}/rendering`}>Rendering with React</Link></li>
+      <li><Link to={`${pathname}/components`}>Components</Link></li>
+      <li><Link to={`${pathname}/props-v-state`}>Props v. State</Link></li>
+    </ul>
+
+    <Match pattern={`${pathname}/:topicId`} component={Topic}/>
+    <Match pattern={pathname} exactly render={() => (
+      <h3>Please select a topic</h3>
+    )}/>
+  </div>
+)
+
+
+const NoMatch = ({ location }) => (
+  <div>Nothing matched {location.pathname}.</div>
+)
+
 
 class App extends Component {
   render() {
@@ -13,6 +55,20 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Match exactly pattern="/" component={Home} />
+          <Match pattern="/about" component={About} />
+          <Match pattern="/topics" component={Topics} />
+          <Miss component={NoMatch}/>
+        </div>
       </div>
     );
   }
